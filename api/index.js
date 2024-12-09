5176ba75169d0dc9fbcef6ad28f7a16e39411b15
Vasanth-FSD-Developer <vasanthkumarmba11@gmail.com>
2024-12-09 13:12:44 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,15 +1,20 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
-mongoose.connect (process.env.MONGO).then (()=>{console.log("Connected to mongodb!")
-}).catch((err)=>{console.log(err);
-})
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to mongodb!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen (3000 , ()=> {
-    console.log('Server running on 3000..!');
-    
-
+app.listen(3000, () => {
+  console.log("Server running on 3000..!");
 });
+app.use ("/api/user", userRouter);
