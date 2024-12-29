b249565ca89846de5ce677ccdf6179c98db0aad8
Vasanth-FSD-Developer <vasanthkumarmba11@gmail.com>
2024-12-29 19:09:44 +0530

    complete update user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -23,7 +23,7 @@ export const updateUser = async (req,res,next)=>{
                     avatar: req.body.avatar,
                 }
             }, {new:true})
-            const {password, ...rest} = updateUser._doc
+            const {password, ...rest} = updatedUser._doc
             res.status(200).json(rest)
         }catch(error){
             next(error)
